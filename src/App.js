391c30324f22eecaca1224d0b8e06a6a7d069a6e
Fernando Andrés - Python Start <72671322+aceu890@users.noreply.github.com>
2023-02-17 20:23:22 -0300

    fin de hardcode, empezamos la llamada a la api

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
-const GIFS = ['https://media1.giphy.com/media/K9z3im98oo9Ve/giphy.gif?cid=ecf05e47cevz8eaoxdz0yfbjnatlu3i8eheikanm557z9vi9&rid=giphy.gif&ct=g', 'https://media1.giphy.com/media/EatwJZRUIv41G/giphy.gif?cid=ecf05e47wx3lhajhmmx2gf220x67dunukxx66td2ihjnypce&rid=giphy.gif&ct=g']
+const GIFS = ['https://media4.giphy.com/media/yX8X517TiuMwuO2tdQ/giphy.gif?cid=ecf05e47cjqftvx8fnbpsaie7hm9525tkjylry6in885n8az&rid=giphy.gif&ct=g', 'https://media0.giphy.com/media/kal6CJH9rjCYjo3zuk/giphy.gif?cid=ecf05e4725y09yegi0cncjnsqari2qz1lf8w1ixlr7nc7rnq&rid=giphy.gif&ct=g']
 
 const DIFFERENT_GIFS = [
-  'https://media0.giphy.com/media/Xu3FmS0zWUeu4/giphy.gif?cid=ecf05e47q5f2oqgw20wyx3ytc6joguqmoyowcoh8sji22jdq&rid=giphy.gif&ct=g'
+  'https://media1.giphy.com/media/t7jzGb5JT0B2D7w771/giphy.gif?cid=ecf05e47tfr5zkm2hfhwhb7l7d51kx6gd644vn0o043o0gfo&rid=giphy.gif&ct=g'
 ]
 
 function App() {
 
   const [gifs, setGifs] = useState(GIFS)
+
+  // useEffect se renderiza cada vez que se renderiza el componente
+  useEffect(() => {
+    // console.log('efecto ejecutado');
+    setGifs(DIFFERENT_GIFS) 
+  }, [])
+  
 
 
   return (
@@ -17,9 +24,11 @@ function App() {
       <section className="App-content">
         {
           // recorre el array y renderiza cada imagen
+          // forEach recorre una lista de elementos, pero no devuelve nada, el map devuelve algo
+          // => return implicito
           gifs.map(singleGif => <img src={singleGif} />)
         }
-        <button onClick={() => setGifs(DIFFERENT_GIFS) }>change Panda</button>
+        <button onClick={() => setGifs(DIFFERENT_GIFS) }>change img</button>
       </section>
     </div>
   );
