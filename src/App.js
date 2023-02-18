78f45a86a20852a9f3587a0855efb8458e4f4035
Fernando Andrés - Python Start <72671322+aceu890@users.noreply.github.com>
2023-02-18 02:50:59 -0300

    fetch done, template string done

--- src/App.js
+++ src/App.js
@@ -1,34 +1,32 @@
 import React, {useState, useEffect} from 'react';
 import './App.css';
 
-const GIFS = ['https://media4.giphy.com/media/yX8X517TiuMwuO2tdQ/giphy.gif?cid=ecf05e47cjqftvx8fnbpsaie7hm9525tkjylry6in885n8az&rid=giphy.gif&ct=g', 'https://media0.giphy.com/media/kal6CJH9rjCYjo3zuk/giphy.gif?cid=ecf05e4725y09yegi0cncjnsqari2qz1lf8w1ixlr7nc7rnq&rid=giphy.gif&ct=g']
-
-const DIFFERENT_GIFS = [
-  'https://media1.giphy.com/media/t7jzGb5JT0B2D7w771/giphy.gif?cid=ecf05e47tfr5zkm2hfhwhb7l7d51kx6gd644vn0o043o0gfo&rid=giphy.gif&ct=g'
-]
+const keyword = 'valorant';
+const apiURL = `https://api.giphy.com/v1/gifs/search?api_key=dIJrma20pSU6ymMwWnDbiaT7NFHeAGVa&q=${keyword}&limit=10&offset=0&rating=G&lang=en`;
 
 function App() {
 
-  const [gifs, setGifs] = useState(GIFS)
+  const [gifs, setGifs] = useState([])
 
-  // useEffect se renderiza cada vez que se renderiza el componente
   useEffect(() => {
-    // console.log('efecto ejecutado');
-    setGifs(DIFFERENT_GIFS) 
+    fetch(apiURL)
+    .then(res => res.json())
+    .then(response => {
+      const {data = []} = response
+      if (Array.isArray(data)) {
+        const gifs = data.map(image => image.images.downsized_medium.url)
+        setGifs(gifs)
+      }
+    })
   }, [])
   
-
-
   return (
     <div className="App">
       <section className="App-content">
         {
-          // recorre el array y renderiza cada imagen
-          // forEach recorre una lista de elementos, pero no devuelve nada, el map devuelve algo
-          // => return implicito
+    
           gifs.map(singleGif => <img src={singleGif} />)
         }
-        <button onClick={() => setGifs(DIFFERENT_GIFS) }>change img</button>
       </section>
     </div>
   );
